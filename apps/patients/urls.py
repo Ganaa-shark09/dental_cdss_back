0f4a772d2927fda8943f6da957baba25ab05f9ce
M.Ganesh <gganeshashokmathpati.123@gmail.com>
2026-04-02 21:36:15 +0530

    fix:fixed patient details and update view

--- apps/patients/urls.py
+++ apps/patients/urls.py
@@ -3,5 +3,5 @@ from apps.patients.views import PatientListCreateAPIView, PatientDetailAPIView
 
 urlpatterns = [
     path("", PatientListCreateAPIView.as_view(), name="patient-list-create"),
-    path("<uuid:patient_id>/", PatientDetailAPIView.as_view(), name="patient-detail"),
+    path("<uuid:patient_uuid>/", PatientDetailAPIView.as_view(), name="patient-detail"),
 ]
